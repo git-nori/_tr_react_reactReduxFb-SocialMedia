b2c22a9b90afac29e5fbca4fd7a118b718ed2dcf
HironoriSato <maza.hs5360@gmail.com>
2020-05-06 15:43:29 +0900

    [add]screamsに1件追加するAPIを実装

--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -22,3 +22,25 @@ exports.getScreams = functions.https.onRequest((request, response) => {
     })
     .catch(err => console.error(err))
 });
+
+// firestoreにscreamsを1件登録する
+exports.createScream = functions.https.onRequest((req, res) => {
+  if(req.method !== 'POST'){
+    return res.status(400).json({error: 'Method not allowed'})
+  }
+
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  }
+
+  admin.firestore().collection('screams').add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successful` })
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong' })
+      console.error(err)
+    })
+})
