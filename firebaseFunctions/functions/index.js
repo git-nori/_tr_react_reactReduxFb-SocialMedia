7895f6913bf0fcd0167d9725f569bec77ea7b71f
HironoriSato <maza.hs5360@gmail.com>
2020-05-06 16:19:28 +0900

    [add]firebaseのインストールと設定の追加

--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -3,6 +3,19 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyArE4qi35UcxOa45AMGcb7yLaGyxIF7XJA",
+  authDomain: "socialapp-b408e.firebaseapp.com",
+  databaseURL: "https://socialapp-b408e.firebaseio.com",
+  projectId: "socialapp-b408e",
+  storageBucket: "socialapp-b408e.appspot.com",
+  messagingSenderId: "455358147512",
+  appId: "1:455358147512:web:fb3a294119d52bd8f83d4d"
+};
+
+const firebase = require('firebase')
+firebase.initializeApp(firebaseConfig)
+
 const express = require('express')
 const app = express()
 
