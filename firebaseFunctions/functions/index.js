a03f675eb3d84193899fc7088bf8c2a8b412a707
HironoriSato <maza.hs5360@gmail.com>
2020-05-08 22:20:27 +0900

    [add]screamを1件取得するAPIを実装

--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -9,11 +9,12 @@ const app = express()
 
 const FBAuth = require('./util/fbAuth')
 
-const { getAllScreams, postOneScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream } = require('./handlers/screams')
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
+app.get('/scream/:screamId', FBAuth, getScream)
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
 app.post('/user', FBAuth, getAuthenticatedUser )
