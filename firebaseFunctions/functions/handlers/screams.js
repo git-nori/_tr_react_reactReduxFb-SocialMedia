3f3b2a3fbf7e82bf92556e1cfbaa87e1f210ab24
HironoriSato <maza.hs5360@gmail.com>
2020-05-08 23:58:25 +0900

    [mod]comment登録時にScreamのcommentCountをインクリメントするよう修正

--- firebaseFunctions/functions/handlers/screams.js
+++ firebaseFunctions/functions/handlers/screams.js
@@ -90,6 +90,10 @@ exports.commentOnScream = (req, res) => {
       if (!doc.exists) {
         return res.status(400).json({ error: 'Scream not found' })
       }
+      // ScreamのcommentCountをインクリメント
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 })
+    })
+    .then(() => {
       return db.collection('comments').add(newComment)
     })
     .then(() => {
--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -9,8 +9,21 @@ const app = express()
 
 const FBAuth = require('./util/fbAuth')
 
-const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream } = require('./handlers/screams')
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream
+} = require('./handlers/screams')
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require('./handlers/users')
 
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
@@ -20,7 +33,7 @@ app.post('/scream/:screamId/like', FBAuth, likeScream)
 app.post('/scream/:screamId/unlike', FBAuth, unlikeScream)
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
-app.post('/user', FBAuth, getAuthenticatedUser )
+app.post('/user', FBAuth, getAuthenticatedUser)
 
 app.post('/signup', signup)
 app.post('/login', login)
