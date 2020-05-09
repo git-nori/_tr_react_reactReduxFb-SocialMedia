7ae7c3af4ac595d28c0d7dadf188c2d862e9443e
HironoriSato <maza.hs5360@gmail.com>
2020-05-09 22:37:51 +0900

    [add]ScreamCards orgを実装

---
+++ socialapp/src/features/home/ScreamCards.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Card, CardContent, CardMedia, Typography, makeStyles } from '@material-ui/core'
+
+const ScreamCards = ({screams}) => {
+  const classes = useStyles()
+
+  const renderCard = () => {
+    return screams.map(scream => {
+      const { body, screamId, userHandle, createdAt, likeCount, commentCount, userImage } = scream
+
+      return (
+        <Card className={classes.card}>
+          <CardMedia component="img" image={userImage} title="Profile image" className={classes.image} />
+          <CardContent className={classes.content}>
+            <Typography
+              variant="h5"
+              component={Link}
+              to={`/users/${userHandle}`}
+              color="primary"
+              className={classes.link}
+            >
+              {userHandle}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+            <Typography variant="body1">{body}</Typography>
+          </CardContent>
+        </Card>
+      )
+    })
+  }
+  return (
+    <>
+      {renderCard()}
+    </>
+  )
+}
+
+const useStyles = makeStyles({
+  card: {
+    display: "flex",
+    marginBottom: "1.25rem",
+  },
+  image: {
+    minWidth: "12.5rem",
+    maxWidth: "12.5rem"
+  },
+  content: {
+    padding: "1.5625rem",
+    objectFit: 'cover'
+  },
+  link: {
+    textDecoration: "none",
+  }
+})
+
+export default ScreamCards
