405ea37f0f14e9ef52900d3833600733af5d13b4
HironoriSato <maza.hs5360@gmail.com>
2020-05-09 15:13:57 +0900

    [add]HomePage, LoginPage, SignupPage compのルーティングを設定

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
+import LoginPage from './features/login/LoginPage'
+import HomePage from './features/home/HomePage'
+import SignupPage from './features/signup/SignupPage'
+
+function App () {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
