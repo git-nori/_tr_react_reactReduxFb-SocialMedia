0ea6b06c996750a70c821f28f1b374183c68c620
HironoriSato <maza.hs5360@gmail.com>
2020-05-09 11:17:12 +0900

    [add]その他のAPIを追加 - markNotificationsRead - getUserDetails - getAuthenticatedUser
    [add]フックのAPIを実装
    - createNotificationOnLike
    - deleteNotificationOnUnLike
    - createNotificationOnComment
    - onUserImageChange
    - onScreamDelete

--- firebaseFunctions/functions/handlers/users.js
+++ firebaseFunctions/functions/handlers/users.js
@@ -103,6 +103,23 @@ exports.getAuthenticatedUser = (req, res) => {
       data.forEach(doc => {
         userData.likes.push(doc.data())
       })
+      return db.collection('notification')
+        .where('recipient', '==', req.user.handle)
+        .orderBy('createdAt', 'desc').limit(10).get()
+    })
+    .then(data => {
+      userData.notifications = []
+      data.forEach(doc => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id
+        })
+      })
       return res.json(userData)
     })
     .catch(err => {
@@ -110,6 +127,44 @@ exports.getAuthenticatedUser = (req, res) => {
       return res.status(500).json({ error: err.code })
     })
 }
+
+// Get any user's details
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection("screams")
+          .where("userHandle", "==", req.params.handle)
+          .orderBy("createdAt", "desc")
+          .get();
+      } else {
+        return res.status(404).json({ errror: "User not found" });
+      }
+    })
+    .then((data) => {
+      userData.screams = [];
+      data.forEach((doc) => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
 
 exports.uploadImage = (req, res) => {
   const Busboy = require('busboy')
@@ -176,3 +231,20 @@ exports.addUserDetails = (req, res) => {
       return res.status(500).json({ error: err.code })
     })
 }
+
+exports.markNotificationsRead = (req, res) => {
+  let batch = db.batch();
+  req.body.forEach((notificationId) => {
+    const notification = db.doc(`/notifications/${notificationId}`);
+    batch.update(notification, { read: true });
+  });
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: "Notifications marked read" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
