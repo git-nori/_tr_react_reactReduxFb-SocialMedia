de888a983200310bc9d380c047cc598916e24cd5
HironoriSato <maza.hs5360@gmail.com>
2020-05-09 22:38:02 +0900

    [add]HomePage pagesを実装

--- socialapp/src/features/home/HomePage.jsx
+++ socialapp/src/features/home/HomePage.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import ScreamCards from './ScreamCards'
+
+import { Container, Grid } from '@material-ui/core'
 
 const HomePage = () => {
-  return(
-    <h1>HomePage</h1>
+  const [screams, setScreams] = useState(null)
+
+  useEffect(() => {
+    axios.get('/screams')
+      .then(res => {
+        setScreams(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const renderRecentScreamsMarkup = () => {
+    return !screams ?
+      <p>...loading</p> : <ScreamCards screams={screams} />
+  }
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={8}>{renderRecentScreamsMarkup()}</Grid>
+        <Grid item xs={12} sm={4}>...profile</Grid>
+      </Grid>
+    </Container>
   )
 }
 
