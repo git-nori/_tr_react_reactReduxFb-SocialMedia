ce86112e51c65f67d09c020a2751c37361519051
HironoriSato <maza.hs5360@gmail.com>
2020-05-11 00:03:54 +0900

    [add]token管理とルーティングをApp.jsに実装

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Route, Switch, useHistory } from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
+import axios from 'axios'
 
+import { setAuthenticated, logoutUser, getUserData } from './features/auth/userSlice'
+
+import AuthRoute from './components/AuthRoute'
 import Navbar from './components/Navbar'
-import LoginPage from './features/login/LoginPage'
+import LoginPage from './features/auth/login/LoginPage'
 import HomePage from './features/home/HomePage'
-import SignupPage from './features/signup/SignupPage'
+import SignupPage from './features/auth/signup/SignupPage'
 
 function App () {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const isAuthenticated = useSelector(state => state.user.authenticated)
+
+  // localStorageからtokenを取得
+  const token = localStorage.getItem('FBIdToken')
+  if (token) {
+    const decodedToken = jwtDecode(token)
+    if (decodedToken.exp * 1000 < Date.now()) {
+      // jwtの有効期限が切れている場合
+      dispatch(logoutUser())
+      history.push("/login")
+    } else {
+      dispatch(setAuthenticated())
+      axios.defaults.headers.common['Authorization'] = token
+      dispatch(getUserData())
+    }
+  }
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/signup" component={SignupPage} />
-        </Switch>
-      </Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <AuthRoute
+          exact
+          path="/login"
+          component={LoginPage}
+          isAuthenticated={isAuthenticated}
+          redirectPath={"/"} />
+        <AuthRoute
+          exact
+          path="/signup"
+          component={SignupPage}
+          isAuthenticated={isAuthenticated}
+          redirectPath={"/"} />
+      </Switch>
     </div>
   );
 }
