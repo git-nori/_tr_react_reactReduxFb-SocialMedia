3b269a2346f635de6fc70314e61106fc760bfc00
HironoriSato <maza.hs5360@gmail.com>
2020-05-10 18:50:56 +0900

    [add]user, data, uiのsliceを作成

---
+++ socialapp/src/features/dataSlice.js
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState=""
+
+const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers:{
+
+  }
+})
+
+export default dataSlice.reducer
