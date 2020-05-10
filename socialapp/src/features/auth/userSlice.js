be4860f01bae712a2dc7f44340d9baab87c554b7
HironoriSato <maza.hs5360@gmail.com>
2020-05-10 22:56:00 +0900

    [add]userSliceを実装

--- socialapp/src/features/auth/userSlice.js
+++ socialapp/src/features/auth/userSlice.js
@@ -1,13 +1,69 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-const initialState=""
+import { setErrors, clearErrors, loadingUi } from '../uiSlice'
+
+import axios from 'axios'
+
+const initialState = {
+  authenticated: false,
+  credentials: {},
+  likes: [],
+  notifications: []
+}
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers:{
-
-  }
+  reducers: {
+    setAuthenticated (state, action) {
+      state.authenticated = true
+    },
+    setUnAuthenticated (state, action) {
+      state.authenticated = false
+    },
+    setUser (state, action) {
+      state.authenticated = true
+      // state = action.payload
+    },
+  },
 })
 
+export const {
+  setUser,
+} = userSlice.actions
+
 export default userSlice.reducer
+
+/** thunk actions */
+// ログイン処理
+export const loginUser = (userData, history) => dispatch => {
+  dispatch(loadingUi())
+
+  axios.post('/login', userData)
+    .then(res => {
+      console.log(res.data)
+
+      // localStorageにTokenを保存, axiosのデフォルトヘッダにセット
+      const FBIdToken = `Bearer ${res.data.token}`
+      localStorage.setItem('FBIdToken', FBIdToken)
+      axios.defaults.headers.common['Authorization'] = FBIdToken
+
+      dispatch(getUserData())
+      dispatch(clearErrors())
+
+      history.push('/')
+    })
+    .catch(err => {
+      console.error(err)
+      dispatch(setErrors(err.response.data))
+    })
+}
+
+// ユーザー情報を取得
+export const getUserData = () => dispatch => {
+  axios.get('/user')
+    .then(res => {
+      dispatch(setUser(res.data))
+    })
+    .catch(err => console.error(err))
+}
