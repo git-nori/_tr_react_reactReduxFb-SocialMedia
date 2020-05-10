84885780fb0aaeaf51f47cc6df78b9a9f26518ba
HironoriSato <maza.hs5360@gmail.com>
2020-05-10 22:55:45 +0900

    [add]uiSliceを実装

--- socialapp/src/features/uiSlice.js
+++ socialapp/src/features/uiSlice.js
@@ -1,13 +1,32 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-const initialState=""
+const initialState = {
+  errors: null,
+  loading: false
+}
 
 const uiSlice = createSlice({
   name: 'ui',
   initialState,
-  reducers:{
-
+  reducers: {
+    setErrors (state, action) {
+      state.loading = false
+      state.errors = action.payload
+    },
+    clearErrors (state, action) {
+      state.loading = false
+      state.errors = null
+    },
+    loadingUi (state, action) {
+      state.loading = true
+    },
   }
 })
+
+export const {
+  setErrors,
+  clearErrors,
+  loadingUi
+} = uiSlice.actions
 
 export default uiSlice.reducer
