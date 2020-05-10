e2bc643df3c027a627a932e2d28056916da3de56
HironoriSato <maza.hs5360@gmail.com>
2020-05-10 18:07:16 +0900

    [add]認証用ルートコンポーネントを実装

---
+++ socialapp/src/components/AuthRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const AuthRoute = ({ component: Component, isAuthenticated, redirectPath, ...rest }) => {
+  console.log(redirectPath)
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return isAuthenticated
+          ? <Redirect to={{ pathname: `${redirectPath}`, state: { from: props.location } }} />
+          : <Component {...props} />
+      }}
+    />
+  )
+}
+
+export default AuthRoute
