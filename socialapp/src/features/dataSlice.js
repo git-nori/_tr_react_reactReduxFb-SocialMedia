f5ce0a90a7f5649d61eac405852218bc4c0a4e0e
HironoriSato <maza.hs5360@gmail.com>
2020-05-13 00:06:26 +0900

    [add]dataSliceの実装

--- socialapp/src/features/dataSlice.js
+++ socialapp/src/features/dataSlice.js
@@ -1,13 +1,64 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState=""
+const initialState = {
+  screams: [],
+  scream: {},
+  loading: false
+}
 
 const dataSlice = createSlice({
   name: 'data',
   initialState,
-  reducers:{
-
+  reducers: {
+    loadingData(state, action) {
+      state.loading = true
+    },
+    setScreams(state, action) {
+      state.screams = action.payload
+      state.loading = false
+    },
+    setLikeUnlikeScream(state,action) {
+      let index = state.screams.findIndex(scream => scream.screamId === action.payload.screamId)
+      state.screams[index] = action.payload
+    }
   }
 })
+
+// Get all screams
+export const getScreams = () => dispatch => {
+  dispatch(loadingData())
+  axios.get('/screams')
+    .then(res => {
+      dispatch(setScreams(res.data))
+    })
+    .catch(err => {
+      dispatch(setScreams([]))
+    })
+}
+
+export const likeScream = screamId => dispatch => {
+  dispatch(loadingData())
+  axios.post(`/scream/${screamId}/like`)
+    .then(res => {
+      dispatch(setLikeUnlikeScream(res.data))
+    })
+    .catch(err => console.error(err))
+}
+
+export const unLikeScream = screamId => dispatch => {
+  dispatch(loadingData())
+  axios.post(`/scream/${screamId}/unlike`)
+    .then(res => {
+      dispatch(setLikeUnlikeScream(res.data))
+    })
+    .catch(err => console.error(err))
+}
+
+export const {
+  loadingData,
+  setScreams,
+  setLikeUnlikeScream
+} = dataSlice.actions
 
 export default dataSlice.reducer
