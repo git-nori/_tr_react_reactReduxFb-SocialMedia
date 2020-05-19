a9fa41c56734dec93de3f46553372d8357881215
HironoriSato <maza.hs5360@gmail.com>
2020-05-19 22:56:55 +0900

    [mod]UserPageでのダイアログも未認証ユーザーにも表示するよう修正

--- socialapp/src/features/user/user/UserPage.jsx
+++ socialapp/src/features/user/user/UserPage.jsx
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 
-import { getUserData, getScream } from '../../scream/screamSlice'
+import { likeScream as setLikesByLike, unlikeScream as setLikesByUnlike } from '../userSlice'
+import { getUserData, getScream, deleteScream, likeScream, unlikeScream, thunkSubmitComment } from '../../scream/screamSlice'
+import { clearErrors } from '../../uiSlice'
 
 import StaticProfile from '../../profile/StaticProfile'
 import Scream from '../../scream/Scream'
@@ -11,7 +13,7 @@ import { Grid, Container } from '@material-ui/core'
 
 const UserPage = () => {
   const dispatch = useDispatch()
-  const { screams } = useSelector(state => state.data)
+  const { screams, scream } = useSelector(state => state.data)
   const isAuthenticated = useSelector(state => state.user.authenticated)
   const ui = useSelector(state => state.ui)
   const { handle } = useParams()
@@ -28,19 +30,59 @@ const UserPage = () => {
       .catch(err => console.log(err))
   }, [])
 
+  const hdlDelScream = (screamId) => {
+    dispatch(deleteScream(screamId))
+  }
+
+  const hdlLikeScream = (screamId) => {
+    dispatch(likeScream(screamId))
+    dispatch(setLikesByLike(screamId))
+  }
+
+  const hdlUnlikeScream = (screamId) => {
+    dispatch(unlikeScream(screamId))
+    dispatch(setLikesByUnlike(screamId))
+  }
+
+  const hdlGetScream = (screamId) => {
+    dispatch(getScream(screamId))
+  }
+
+  const hdlSubmitComment = (screamId, data) => {
+    dispatch(thunkSubmitComment(screamId, data))
+  }
+
+  const hdlClearErrors = () => {
+    dispatch(clearErrors())
+  }
+
   const screenMarkup = () => {
-    if (ui.loading) {
+    // 初期表示時
+    if (ui.loading && screams.length < 1) {
       return <p>...loading</p>
     }
 
+    const comments = scream.comments
     return screams === null
       ? (
         <p>No screams from this user</p>
       )
       : (
         screams.map((scream) => {
-          return <Scream scream={scream} key={scream.screamId} authenticated={isAuthenticated} />
-        })
+          return <Scream 
+          key={scream.screamId} 
+          scream={scream} 
+          authenticated={isAuthenticated} 
+          comments={comments}
+          likeScream={hdlLikeScream}
+          unlikeScream={hdlUnlikeScream}
+          deleteScream={hdlDelScream}
+          getScream={hdlGetScream}
+          submitComment={hdlSubmitComment}
+          clearErrors={hdlClearErrors}
+          errors={ui.errors}
+          loading={ui.loading} />
+          })
       )
   }
 
